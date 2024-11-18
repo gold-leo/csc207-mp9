2dbfc8a38975ee68eece8419d492a53f8b49e8f2
gold-leo <goldmanleo@grinnell.edu>
2024-11-17 20:28:25 -0600

    Added SimpleValidator

---
+++ src/main/java/edu/grinnell/csc207/blockchains/SimpleValidator.java
@@ -0,0 +1,20 @@
+package edu.grinnell.csc207.blockchains;
+
+/**
+ * Things that validate hashes. Simple version.
+ *
+ * @author Leo Goldman
+ */
+public class SimpleValidator implements HashValidator {
+  /**
+   * Determine if a hash meets some criterion.
+   *
+   * @param hash
+   *   The hash we're checking.
+   *
+   * @return true if the hash is valid and false otehrwise.
+   */
+  public boolean isValid(Hash hash) {
+    return (hash.length() >= 1) && (hash.get(0) == 0);
+  }
+} // SimpleValidator
